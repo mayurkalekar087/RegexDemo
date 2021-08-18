952143f09f274f0f9d28e3f28b36dc7e058b6c4b
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-18 19:12:17 +0530

    feat:RegexDemo

---
+++ RegexPatternMatchingDemo/Patterns.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPatternMatchingDemo
+{
+    public class Patterns
+    {
+        public static string REGEX_PINCODE = "^[1-9][0-9]{5}$";
+        public bool validatePinCode(string pincode)
+        {
+            return Regex.IsMatch(pincode, REGEX_PINCODE);
+        }
+    }
+}
---
+++ RegexPatternMatchingDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RegexPatternMatchingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Patterns patterns = new Patterns();
+            Console.WriteLine(patterns.validatePinCode("443001"));
+        }
+    }
+}
